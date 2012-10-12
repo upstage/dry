4d9b781e2c21e095acc2ad4256f19366959896cd
Brian Woodward <brian.woodward@gmail.com>
2012-10-12 15:51:53 -0400

    setting up initial use of node-parselib for getting information about css files

--- grunt.js
+++ grunt.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
       files: ['test/**/*.js']
     },
     lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+      files: ['grunt.js', 'lib/dry.js', 'test/**/*.js']
     },
     watch: {
       files: '<config:lint.files>',
