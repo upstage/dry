109c092f40cd9f8bedd461e1d29fe7672d6bacb5
Jon Schlinkert <github@sellside.com>
2013-03-06 01:11:47 -0500

    new gruntfile, gitignore, .jshintrc

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+              '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+              '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+              '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+              ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+
+    nodeunit: {
+      files: ['test/**/*_test.js'],
+    },
+    jshint: {
+      options: { jshintrc: '.jshintrc' },
+      files: ['lib/**/*.js', 'test/**/*.js', 'Gruntfile.js']
+    },
+
+    concat: {
+      dist: {
+        src: [
+          '<banner:meta.banner>',
+          '<file_strip_banner:lib/dry.js>',
+          'src/dry.js',
+          'src/rules/*.js',
+          'src/build/footer.js'
+        ],
+        dest: 'tasks/dry.js'
+      }
+    },
+
+    uglify: {
+      options: { mangle: false },
+      plugin: {
+        files: {
+          'tasks/dry.min.js': ['<%= concat.dest %>']
+        }
+      }
+    },
+
+    dry: {
+      // options: {
+      //   variables: true,
+      //   nesting: true
+      // },
+      tests: {
+        files: {
+          'test/results/test.less': ['test/fixtures/bootstrap.css']
+        }
+      }
+    },
+
+    watch: {
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-recess');
+
+  // Load local tasks.
+  grunt.loadTasks('tasks');
+
+  // Default task.
+  grunt.registerTask('default', [
+    'concat',
+    'uglify',
+    'test'
+  ]);
+
+  // Tests to be run.
+  grunt.registerTask('test', [
+    'nodeunit',
+    'jshint',
+    'dry:tests'
+  ]);
+};
