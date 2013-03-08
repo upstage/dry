0ecaa09083acb90990d097668afca8fb005c950f
Brian Woodward <brian.woodward@gmail.com>
2013-03-07 23:57:33 -0500

    updating dry task to be grunt 0.4.0 compatible

--- tasks/dry.js
+++ tasks/dry.js
@@ -1,7 +1,34 @@
-
-'use strict';
-
 module.exports = function(grunt) {
+  'use strict';
+  var dry = require('../lib/dry');
 
+  grunt.registerMultiTask('dry', 'Refactor CSS files into LESS files.', function() {
+
+    grunt.verbose('Processing all files...');
+
+    var done = this.async();
+    var fileCount = this.files.length;
+    var filesComplete = 0;
+
+    this.files.forEach(function(filePair) {
+
+      var src = filePair.src;
+      src.forEach(function(filepath){
+        var source = filepath;
+        var destination = source.replace(/\.css/, '.less');
+
+        dry.refactor(source, destination, function(errorCount){
+          grunt.verbose('Errors: ' + errorCount);
+          // Otherwise, print a success message....
+          grunt.verbose('File "' + destination + '" created.');
+          filesComplete++;
+
+          if(filesComplete >= fileCount){
+            done();
+          }
+        });
+      });
+    });
+  });
 
 };
